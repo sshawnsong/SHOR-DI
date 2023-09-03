fc5d909ee5ddb6d67337a12999be9395b0de7448
Shawn Song <shawnsong@Shawns-MacBook-Pro.local>
2023-09-03 19:12:43 +0900

    Add Middleware

--- api/main.py
+++ api/main.py
@@ -4,6 +4,16 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
